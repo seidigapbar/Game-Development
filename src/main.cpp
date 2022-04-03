bf4b50882d9a59248a13e5d484402e91f336eb3f
Sanzhar <sanzharseidigapbar@gmail.com>
2022-04-03 18:27:00 +0200

    Got rid of the merge consequences. Finally

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
-#include <iostream>
-#include <vector>
+#include "Game.h"
+
+// our Game object
+Game* g_game = 0;
 
 int main()
 {
-	std::cout<<"Hello, world! \n";
+	g_game = new Game();
+
+	g_game->init("Chapter 1", 100, 100, 640, 480, false);
+
+	while(g_game->running())
+	{
+		g_game->handleEvents();
+		g_game->update();
+		g_game->render();
+	}
+	g_game->clean();
+
 	return 0;
 }
+
